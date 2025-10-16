049efee5908a0477f5ea05c5f7ebdacb5ba93df0
ayannath12 <ayandebnath550@gmail.com>
2025-10-17 00:36:07 +0600

    test 3

---
+++ lib/dynamicVar.dart
@@ -0,0 +1,19 @@
+void main() {
+  var id = 4;
+  id = 5;
+  print(id);
+
+  //Dynamic concept of var
+
+  var name;
+  name = 10;
+  name = 'ayan';
+
+  print(name);
+
+  dynamic obj;
+  obj = 'akkk';
+  obj = 6;
+  obj = true;
+  print(obj);
+}
