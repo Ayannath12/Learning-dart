cb793824fc274f37a00651592952015c0d0a6a3b
ayannath12 <ayandebnath550@gmail.com>
2025-10-17 21:13:42 +0600

    test8

---
+++ lib/finalConst.dart
@@ -0,0 +1,21 @@
+void main() {
+  //Final use korle reassign kora jabe na
+  final a;
+  a = 'rabin';
+
+  final String b;
+  b = 'dfs';
+
+  final names = ['roni', 'rakib', 'sakib'];
+  names.add('jakib');//add hobe
+  print(names);
+
+  //const
+  const d = 'fdg';
+
+  const name = ['roni', 'rakib', 'sakib'];
+  names.add('jakib');//add hobe na
+  print(name);
+
+
+}
